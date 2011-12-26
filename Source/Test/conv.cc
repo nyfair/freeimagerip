7fc3951f1f1cbe9df50de1bfe713f4ae844f1c8d
nyfair <nyfair2012@gmail.com>
2011-12-26 10:29:41 +0800

    remove cmake's asm define about zlib for non-vc compiler

---
+++ Source/Test/conv.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "FreeImage.h"
+
+int main(int argc, char *argv[]) {
+	if(argc != 3) {
+		printf("Usage : convert <input file name> <output file name>\n");
+		return 0;
+	}
+	FREE_IMAGE_FORMAT fifin = FreeImage_GetFIFFromFilename(argv[1]);
+	FREE_IMAGE_FORMAT fifout = FreeImage_GetFIFFromFilename(argv[2]);
+	FIBITMAP *dib = FreeImage_Load(fifin, argv[1]);
+	FreeImage_Save(fifout, dib, argv[2]);
+	FreeImage_Unload(dib);
+	return 0;
+}
